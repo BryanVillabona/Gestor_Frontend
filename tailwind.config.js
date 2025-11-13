369f48a0e5688bd6e669cc37bee2980d19a595b4
BryanVillabona <bvillabona13@gmail.com>
2025-11-13 17:56:52 -0500

    feat: :sparkles: bug
    @BryanVillabona

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,35 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  
+  // ¡AQUÍ ESTÁ LA OTRA PARTE CLAVE!
+  darkMode: "class", 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Color principal (amarillo)
+        "primary": "#FFC107", 
+        
+        // Color de fondo
+        "background-light": "#f6f7f8",
+        "background-dark": "#101a22",
+        
+        // Colores de los otros mockups (Ventas, Clientes)
+        "primary-blue": "#1392ec", // El azul
+        "success": "#2ecc71",
+        "danger": "#e74c3c", // Para eliminar
+        "verde-stock": "#28A745", // El verde de "Añadir Stock"
+
+        // Colores de texto
+        "text-main": "#111518",
+        "text-muted": "#617989",
+        "border-light": "#dbe1e6",
+      },
+      fontFamily: {
+        // La fuente "Inter"
+        "display": ["Inter", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
